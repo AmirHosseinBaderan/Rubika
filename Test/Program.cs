7bc59afd451d0be86c399278cdcd0cf71ce51ac7
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-16 11:42:05 -0800

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Rubika.Package.Bot;
 
-IBot _bot = new Bot("krirsqxchedzqgmkjvxhhujyysgshkbj");
+IBot _bot = new Bot("auth");
 
 await _bot.CreateBotAsync((message) =>
 {
     Console.WriteLine(message.Id);
-}, "g0qSWk0ee31cd431a7ac489106fcf62a");
+}, "gapToken");
