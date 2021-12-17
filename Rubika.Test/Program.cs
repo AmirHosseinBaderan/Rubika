4c95b5cc5b6b20d864781ba7bf41b27cd1f68f14
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-17 10:52:22 -0800

    update exam

---
+++ Rubika.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using Rubika.Package.Bot;
+
+string _auth = ".";
+string _gapToken = ".";
+
+IBot _bot = new Bot(_auth);
+
+await _bot.CreateBotAsync(async (message) =>
+{
+    if (message.Status == ActionStatus.Success)
+    {
+        Int32 unixTimestamp = (Int32)(DateTime.Now.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        var chats = await _bot.GetChatsUpdatesAsync(unixTimestamp.ToString());
+        if (chats.Status == ActionStatus.Success)
+            foreach (var chat in chats.Chats)
+                Console.WriteLine(chat.ObjectGuid);
+
+    }
+    else
+        Console.WriteLine("exception");
+
+}, _gapToken);
