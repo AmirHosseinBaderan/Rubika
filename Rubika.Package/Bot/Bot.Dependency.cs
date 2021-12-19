16a3bac9da5d5f6d34e591deffbc72dd78c954e0
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-19 01:06:35 -0800

    Create Dependency Partial

---
+++ Rubika.Package/Bot/Bot.Dependency.cs
@@ -0,0 +1,25 @@
+﻿namespace Rubika.Package.Bot;
+
+public partial class Bot : IBot, IDisposable
+{
+
+    private Action<GetMessage> _onGetMessage;
+
+    private string _gapToken;
+
+    private readonly string _auth;
+
+    private readonly IApi _api;
+
+    private readonly List<Message> _messages;
+
+    private readonly string _url = "https://messengerg2c63.iranlms.ir";
+
+    public Bot(string auth)
+    {
+        _auth = auth;
+        _auth.CreateAndSetKey();
+        _messages = new();
+        _api = new Api.Api();
+    }
+}
