032093f1084cb8a37e21f6f81b0e4be44707e978
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-19 05:19:38 -0800

    create channel info

--- Rubika.Package/Bot/Bot.Chat.cs
+++ Rubika.Package/Bot/Bot.Chat.cs
@@ -5,7 +5,9 @@ public partial class Bot : IBot, IDisposable
     public async Task SeenCahtAsync(SeenChat seenChat)
            => await Task.Run(async () =>
            {
-               JObject input = JObject.Parse("{\"seen_list\" : {\"" + seenChat.GapToken + "\":\"" + seenChat.MessageId + "\"}}");
+               string gapToken = seenChat.GapToken ?? _gapToken;
+
+               JObject input = JObject.Parse("{\"seen_list\" : {\"" + gapToken + "\":\"" + seenChat.MessageId + "\"}}");
                string v4Data = await CreateDataV4Async(input.ToString(), "seenChats", _auth);
                await _api.SendRequestAsync(_url, v4Data.GetBytes());
            });
@@ -35,4 +37,34 @@ public partial class Bot : IBot, IDisposable
                     return new GetUpdatesChats(ActionStatus.Exception, null);
                 }
             });
+
+    public async Task<JObject> GetObjectByUserNameAsync(string userName)
+        => await Task.Run(async () =>
+        {
+            string json = new JObject
+            {
+                {"username",userName }
+            }.ToString();
+            string v4Data = await CreateDataV4Async(json, "getObjectByUsername", _auth);
+            string request = await _api.SendRequestAsync(_url, v4Data.GetBytes());
+            return await _api.ConvertToJObjectAsync(request);
+        });
+
+    public async Task<GetChannelByUserName> GetChannelByUserNameAsync(string userName)
+        => await Task.Run(async () =>
+        {
+            JObject getObject = await GetObjectByUserNameAsync(userName);
+            if (!getObject.ContainsKey("err"))
+            {
+                Channel channel = CreateChannel(getObject["channel"]);
+                return new GetChannelByUserName(channel)
+                {
+                    Status = ActionStatus.Success,
+                    Chat = CreateChat(getObject["chat"]),
+                    Exist = bool.Parse(getObject["exist"]?.ToString() ?? "false"),
+                    Type = getObject["type"]?.ToString()
+                };
+            }
+            return new GetChannelByUserName(null) { Status = ActionStatus.Exception };
+        });
 }
