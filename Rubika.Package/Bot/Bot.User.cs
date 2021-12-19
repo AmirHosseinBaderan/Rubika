1f03e25aae0588479d3629f7e8a1aba80b5043a3
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-19 05:19:09 -0800

    get user info

--- Rubika.Package/Bot/Bot.User.cs
+++ Rubika.Package/Bot/Bot.User.cs
@@ -9,14 +9,7 @@ public partial class Bot : IBot, IDisposable
        string request = await _api.SendRequestAsync(_url, createData.GetBytes());
        JObject resObject = await _api.ConvertToJObjectAsync(request);
        JToken user = resObject["user"];
-       return new UserInof()
-       {
-           Name = user["first_name"]?.ToString(),
-           LastName = user["last_name"]?.ToString(),
-           Bio = user["bio"]?.ToString(),
-           UserName = user["username"]?.ToString(),
-           UserGuid = user["user_guid"]?.ToString()
-       };
+       return CreateUserInfo(user);
    });
 
     public async Task<string> GetGuidFromUserNameAsync(string userName)
@@ -31,6 +24,8 @@ public partial class Bot : IBot, IDisposable
     public async Task RemoveUserAsync(string userToken, string gapToken)
         => await Task.Run(async () =>
         {
+            if (gapToken == null) gapToken = _gapToken;
+
             JObject json = new()
             {
                 { "action", "Set" },
@@ -44,6 +39,8 @@ public partial class Bot : IBot, IDisposable
     public async Task UnRemoveUserAsync(string userToken, string gapToken)
           => await Task.Run(async () =>
           {
+              if (gapToken == null) gapToken = _gapToken;
+
               JObject json = new()
               {
                   { "action", "Unset" },
@@ -57,6 +54,8 @@ public partial class Bot : IBot, IDisposable
     public async Task NewAdminAsync(string userToken, IEnumerable<string> access, string gapToken)
             => await Task.Run(async () =>
             {
+                if (gapToken == null) gapToken = _gapToken;
+
                 JArray accessList = new();
                 foreach (string ac in access)
                     accessList.Add(ac);
@@ -76,6 +75,8 @@ public partial class Bot : IBot, IDisposable
     public async Task RemoveAdminAsync(string adminToken, string gapToken)
         => await Task.Run(async () =>
         {
+            if (gapToken == null) gapToken = _gapToken;
+
             JObject json = new()
             {
                 { "action", "UnsetAdmin" },
@@ -85,5 +86,23 @@ public partial class Bot : IBot, IDisposable
 
             string v5Data = await CreateDataV5Async(json.ToString(), "setGroupAdmin", _auth);
             await _api.SendRequestAsync(_url, v5Data.GetBytes());
+        });
+
+    public async Task<GetUserByUserName> GetUserByUserNameAsync(string userName)
+        => await Task.Run(async () =>
+        {
+            JObject getObject = await GetObjectByUserNameAsync(userName);
+            if (!getObject.ContainsKey("err"))
+            {
+                UserInof user = CreateUserInfo(getObject["user"]);
+                return new GetUserByUserName(user)
+                {
+                    Exist = bool.Parse(getObject["exist"]?.ToString() ?? "false"),
+                    Status = ActionStatus.Success,
+                    Type = getObject["type"]?.ToString(),
+                    Chat = CreateChat(getObject["chat"])
+                };
+            }
+            return new GetUserByUserName(null) { Status = ActionStatus.Exception };
         });
 }
