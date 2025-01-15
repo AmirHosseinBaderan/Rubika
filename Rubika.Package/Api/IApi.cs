edb0be72ecc2fa0d782278cc4dc6e120fd5ea91b
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2025-01-15 13:46:14 +0330

    update : delete last version

--- Rubika.Package/Api/IApi.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rubika.Package.Api;
-
-internal interface IApi
-{
-    Task<string> SendRequestAsync(string url, byte[] data);
-
-    Task<JObject> ConvertToJObjectAsync(string response);
-}
